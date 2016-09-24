d5d71bf68551751a587dacd9f5dfcfe01fe42048
Sashik Zatynin <tochcha@gmail.com>
2016-09-24 18:32:31 +0300

    prosto tak

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// для html 5
+// для html 5 end
+document.createElement('header');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('footer');
+document.createElement('figure');
+document.createElement('figcaption');
+// для html 5 end
+
+// для первого скролла
+$(document).ready(function(){
+	$('div.G1').galleryScroll();
+});
+// для первого скролла end
+
+// для второго скролла
+$(document).ready(function(){
+	$('div.G2').galleryScroll();
+});
+// для второго скролла end
+
+
+
